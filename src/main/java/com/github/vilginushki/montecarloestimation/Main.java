02df2cf1843ec2113ca2a2393093faf139f692bf
Vilginushki <hubert.wila@gmail.com>
2022-10-07 12:29:15 +0200

    Add maven support

--- src/main/java/com/github/vilginushki/montecarloestimation/Main.java
+++ src/main/java/com/github/vilginushki/montecarloestimation/Main.java
@@ -9,18 +9,18 @@ public class Main {
     private static double simulate(int points, int radius) {
         int inCircle = 0;//Count points in circle
         for (int i = 0; i < points; i++) {
-            double coordX = (Math.random() - 0.5) * 2 * radius;//Generate random coordinates for point
-            double coordY = (Math.random() - 0.5) * 2 * radius;
+            double cordX = (Math.random() - 0.5) * 2 * radius;//Generate random coordinates for point
+            double cordY = (Math.random() - 0.5) * 2 * radius;
 
-            if ((Math.pow(coordX, 2) + Math.pow(coordY, 2)) <= radius * radius) {//check if point is in circle
+            if ((Math.pow(cordX, 2) + Math.pow(cordY, 2)) <= radius * radius) {//check if point is in circle
                 inCircle++;
             }
         }
-        return 4 * ((double) inCircle / points);//return pi estimation
+        return (4 * ((double) inCircle / points));//return pi estimation
 
     }
 
     public static void main(String[] args) {
-        System.out.printf("Estimated value of PI is: %6f\n", simulate((int) (100000000), 2));
+        System.out.printf("Estimated value of PI is: %6f\n", simulate((int) (1000000000), 1));//print pi estimation
     }
 }
